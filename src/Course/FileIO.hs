d003dbd9e9f7bbafb2ffe5a6a23e02f589edc92a
David Tchepak <dave@davesquared.net>
2015-09-15 15:54:41 +1000

    FileIO exercise
    Had to copy ./share/* into ./ and run from ghci to test:
    ghci> Course.FileIO.main' ("files.txt":.Nil)

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE TupleSections #-}
 
 module Course.FileIO where
 
@@ -11,6 +12,9 @@ import Course.Apply
 import Course.Bind
 import Course.Functor
 import Course.List
+-- My imports:
+import Course.Optional (Optional(..), (??))
+import Course.Traversable
 
 {-
 
@@ -62,8 +66,19 @@ the contents of c
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = getArgs >>= main'
+
+single :: List a -> Optional a
+single (x:.Nil) = Full x
+single _ = Empty
+
+-- For testing arg handling from ghci
+main' ::
+  List Chars
+  -> IO ()
+main' args =
+  single (run <$> args) ?? putStrLn "Usage: runhaskell FileIO.hs (filename)"
+
 
 type FilePath =
   Chars
@@ -72,31 +87,33 @@ type FilePath =
 run ::
   Chars
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run file =
+  let files = lines <$> readFile file
+  in files >>= (printFiles <=< getFiles)
 
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
 getFiles =
-  error "todo: Course.FileIO#getFiles"
+  traverse getFile -- sequence . map getFile
 
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile path =
+  (path,) <$> readFile path
 
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
 printFiles =
-  error "todo: Course.FileIO#printFiles"
+  void . traverse (uncurry printFile)
 
 printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile path contents =
+  putStrLn ("============ " ++ path)
+  >> putStrLn contents
 
