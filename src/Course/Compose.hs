2fed8d10f4889ced2a9dca3d273e86ca70b3619b
David Tchepak <dave@davesquared.net>
2015-09-17 14:44:30 +1000

    Compose exercise

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -14,26 +14,22 @@ import Course.Bind
 newtype Compose f g a =
   Compose (f (g a))
 
--- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  f <$> Compose c =
+    Compose $ ((<$>) . (<$>)) f c
 
 instance (Apply f, Apply g) =>
   Apply (Compose f g) where
--- Implement the (<*>) function for an Apply instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  Compose f <*> Compose a =
+    Compose $ lift2 (<*>) f a
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
--- Implement the pure function for an Applicative instance for Compose
   pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+    Compose . pure . pure
 
 instance (Bind f, Bind g) =>
   Bind (Compose f g) where
--- Implement the (=<<) function for a Bind instance for Compose
   (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+    undefined
