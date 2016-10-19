91c7689a77b1761fed8ed737b5bd05ac4690329a
David Tchepak <dave@davesquared.net>
2016-10-19 12:15:07 +1100

    Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -16,20 +16,21 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  f <$> Compose c =
+    Compose $ (fmap . fmap) f c
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
   pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+    Compose . pure . pure
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  -- Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  Compose f <*> Compose c =
+    Compose (lift2 (<*>) f c)
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
   (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+    undefined
