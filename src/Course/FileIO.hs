052633a784fb6363a97de351ea82978023565495
David Tchepak <dave@davesquared.net>
2018-04-12 15:16:46 +1000

    FileIO before refactoring

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE TupleSections #-}
 
 module Course.FileIO where
 
@@ -85,8 +86,9 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile path contents =
+    putStrLn ("============ " ++ path)
+    >> putStrLn contents
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
@@ -94,15 +96,15 @@ printFiles ::
   List (FilePath, Chars)
   -> IO ()
 printFiles =
-  error "todo: Course.FileIO#printFiles"
+  void . sequence . (<$>) (uncurry printFile)
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile p =
+  (p,) <$> readFile p
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
@@ -110,7 +112,7 @@ getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
 getFiles =
-  error "todo: Course.FileIO#getFiles"
+  sequence . (<$>) getFile
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
@@ -118,13 +120,14 @@ run ::
   FilePath
   -> IO ()
 run =
-  error "todo: Course.FileIO#run"
+  let files = (lines <$>) . readFile
+  in (=<<) (printFiles <=< getFiles) . files
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
 main =
-  error "todo: Course.FileIO#main"
+  getArgs >>= void . sequence . ((<$>) run)
 
 ----
 
